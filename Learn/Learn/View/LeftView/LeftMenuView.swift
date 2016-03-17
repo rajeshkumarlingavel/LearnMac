149fa42f745182f91e2b11243df935e0d3a0473d
rajeshkumar.l <rajeshkumar.l@zohocorp.com>
2016-03-18 00:40:03 +0530

    tabelView with bug

---
+++ Learn/Learn/View/LeftView/LeftMenuView.swift
@@ -0,0 +1,19 @@
+//
+//  LeftMenuView.swift
+//  Learn
+//
+//  Created by rajeshkumar.l on 17/03/16.
+//  Copyright © 2016 rajesh-2098. All rights reserved.
+//
+
+import Cocoa
+
+class LeftMenuView: NSView {
+
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
---
+++ Learn/Learn/ViewController/MainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewController.swift
+//  Learn
+//
+//  Created by rajeshkumar.l on 17/03/16.
+//  Copyright © 2016 rajesh-2098. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: NSViewController {
+
+}
