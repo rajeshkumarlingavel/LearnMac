de84658f8fb75bed60169209b5d9f0fce91cad00
rajeshkumar.l <rajeshkumar.l@zohocorp.com>
2016-03-17 21:58:55 +0530

    init

---
+++ Learn/Learn/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Learn
+//
+//  Created by rajeshkumar.l on 17/03/16.
+//  Copyright © 2016 rajesh-2098. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
